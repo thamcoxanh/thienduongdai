c3860e4b533dc929368800b3597f07d181368969
Luu <luunguyenthanh91@gmail.com>
2019-04-11 11:27:18 +0700

    ok

--- V1/README.md
+++
@@ -1,2 +0,0 @@
-# thienduongdai
-thien duong dai
--- V1/components/bk.js
+++
@@ -1,7 +0,0 @@
-<TouchableOpacity onPress={() => {
-    this.props.navigation.navigate('');
-}}>
-<View style={[Styles.content_items,{marginBottom:10,borderRadius:5,borderWidth:0.5}]}>
-    <Text style={Styles.content_items_title}>Các bài thiền đã mua</Text>
-</View>
-</TouchableOpacity>
